0d2b7078735845aaf0d7f9728a24c7bcb467c1bd
urielBarhum <uryel.uryel7890@gmail.com>
2023-12-24 23:26:01 +0200

    שיונוי תצוגת המוצרים והתחלה של יצירת הזמנה על ידי הלקוח

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Observable } from 'rxjs';
+import { orderRoomService } from 'src/app/models/orderRoomService';
 import { Product } from 'src/app/models/product';
+import { OrderRoomServiceService } from 'src/app/services/order-room-service.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,16 +12,17 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-  @Input() product !:Product
-  constructor(private productService: ProductService, http: HttpClient) { }
-add:number = 0;
-  addOne(){
+  @Input() product !: Product
+  constructor(private productService: ProductService, http: HttpClient, private orderRoomServices: OrderRoomServiceService) { }
+  add: number = 0;
+  addOne() {
     this.add++;
+    this.orderRoomServices.addOrderRoomService(this.product.iDProduct)
   }
-  subOne(){
-    this.add --;
+  subOne() {
+    this.add--;
   }
-  removAll(){
+  removAll() {
     this.add = 0;
   }
 }
--- src/app/services/order-room-service.service.ts
+++ src/app/services/order-room-service.service.ts
@@ -2,16 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { orderRoomService } from '../models/orderRoomService';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderRoomServiceService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient , private router: Router) {
 
   }
-  getORS(): Observable<orderRoomService[]> {
-    return this.http.get<orderRoomService[]>('https://localhost:44382/api/OrderRoomServic/GetORS')
+  token: string =""
+  getOrdersRoomServices(): Observable<orderRoomService[]> {
+    return this.http.get<orderRoomService[]>('https://localhost:44382/api/OrderRoomServic/GetOrderRoomService')
   }
+  addOrderRoomService (productID : number) : Observable<string>{
+    return this.http.post('https://localhost:44382/api/OrderRoomServic/AddOrderRoomService' , productID ,{ responseType: "text" })
+  }
+
 }
