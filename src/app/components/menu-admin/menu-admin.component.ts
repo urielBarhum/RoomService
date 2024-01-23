ddb307fe2cab42f78c4f4b9fa9a932bc6a3a2188
urielBarhum <uryel.uryel7890@gmail.com>
2024-01-23 22:31:21 +0200

    הוספה של תמונות וכרטיס כניסה לטבלאות של המנהל

--- src/app/components/menu-admin/menu-admin.component.ts
+++ src/app/components/menu-admin/menu-admin.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-admin',
   templateUrl: './menu-admin.component.html',
   styleUrls: ['./menu-admin.component.scss']
 })
-export class MenuAdminComponent {
+export class MenuAdminComponent implements OnInit {
+
+  constructor(private router : Router){}
+  ngOnInit(): void {
+    
+  }
+  oreders(){
+    this.router.navigateByUrl('tableCustumers')
+  }
 
 }
